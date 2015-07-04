64d89937c43a4696c7b6248ae830c3306ce276e8
Ryan Joy <atxryan@gmail.com>
2015-07-04 10:24:08 -0500

    Adding node type definition

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-﻿var express = require('express');
+﻿/// <reference path="typings/node/node.d.ts"/>
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
